72d802d892ef038f20daf15d06b496b9e13300d5
barackmaund1 <pmaundu49@gmail.co>
2020-08-06 12:06:46 +0300

    first commit and set firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEAx-WR6eiEdg8AWUPE90Q7qwslVKgABM",
+    authDomain: "tik-tok-clone-9c126.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-9c126.firebaseio.com",
+    projectId: "tik-tok-clone-9c126",
+    storageBucket: "tik-tok-clone-9c126.appspot.com",
+    messagingSenderId: "223687453247",
+    appId: "1:223687453247:web:f3d305f5d19e7735bd668a"
+  };
