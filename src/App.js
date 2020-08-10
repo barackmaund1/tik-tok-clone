8b0a60f10d4c6b904a9e227f6c571bbfca6e26a0
barackmaund1 <pmaundu49@gmail.co>
2020-08-10 13:39:01 +0300

    create real-time database to tik tok app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 import Video from './Video';
+import db from './firebase';
 
 function App() {
+
+  const [videos,setVideos] = useState([])
+
+  useEffect(() => {
+    db.collection('videos').onSnapshot((snapshot) =>
+      setVideos(snapshot.docs.map(doc => doc.data())
+      ))
+    
+  },[])
+
   return (
 
     <div className="App">
       
       <div className='app__videos'>
-         <Video
-         url='https://v16m.tiktokcdn.com/18a37eb09f967de8547b218247d943c9/5f330202/video/tos/useast2a/tos-useast2a-ve-0068c002/0d0bf826cb45499a9ed2d42103941f03/?a=1233&br=2344&bt=1172&cr=0&cs=0&dr=0&ds=3&er=&l=2020080920392901018907414857428916&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=amh2aHFkeDpvdTMzNjczM0ApMzxkZmg2OGVmN2g4ZWU5OGdrNl9pZGBnXy1fLS1eMTZzczQwNTIvLTNeNDMtNDViMzU6Yw%3D%3D&vl=&vr='
-         channel='barack'
-         description='wow this works...'
-         song='the sunny barack '
-         likes={222}
-         messages={400}
-         shares={200}
-         />
-         <Video/>
-         <Video/>
-         <Video/>
+      {videos.map(({url,channel,description,song,messages,likes,shares}) =>(
+        <Video 
+        url={url}
+        channel={channel}
+        song={song}
+        likes={likes}
+        messages={messages}
+        description={description}
+        shares={shares}
+        />
+      ))}
+         
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,18 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDEAx-WR6eiEdg8AWUPE90Q7qwslVKgABM",
-    authDomain: "tik-tok-clone-9c126.firebaseapp.com",
-    databaseURL: "https://tik-tok-clone-9c126.firebaseio.com",
-    projectId: "tik-tok-clone-9c126",
-    storageBucket: "tik-tok-clone-9c126.appspot.com",
-    messagingSenderId: "223687453247",
-    appId: "1:223687453247:web:f3d305f5d19e7735bd668a"
-  };
+  apiKey: "AIzaSyDEAx-WR6eiEdg8AWUPE90Q7qwslVKgABM",
+  authDomain: "tik-tok-clone-9c126.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-9c126.firebaseio.com",
+  projectId: "tik-tok-clone-9c126",
+  storageBucket: "tik-tok-clone-9c126.appspot.com",
+  messagingSenderId: "223687453247",
+  appId: "1:223687453247:web:f3d305f5d19e7735bd668a"
+};
+  
+const firebaseApp =
+firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
